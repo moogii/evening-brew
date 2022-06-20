d965b64d64b11bb360bcf25a3fcfe36a96583b38
Munkhbaatar Ganbaatar <ganbaa.moogii@gmail.com>
2022-06-20 16:43:43 +0800

    topic action test done

--- blog-backend/src/topic-actions/topic-actions.service.int-spec.ts
+++ blog-backend/src/topic-actions/topic-actions.service.int-spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Subscriber, Topic } from '@prisma/client';
 import { AppModule } from '../app.module';
+import { PrismaService } from '../prisma/prisma.service';
 import { TopicActionsService } from './topic-actions.service';
 
 describe('TopicActionsService', () => {
   let service: TopicActionsService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,9 +14,37 @@ describe('TopicActionsService', () => {
     }).compile();
 
     service = module.get<TopicActionsService>(TopicActionsService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  let topic: Topic = null;
+  let subscriber: Subscriber = null;
+
+  it('should create a topic', async () => {
+    topic = await prisma.topic.create({
+      data: {
+        name: 'Test topic',
+        slug: 'test-topic',
+      }
+    });
+    expect(topic).toBeDefined();
+  });
+
+  it('should create a subscriber', async () => {
+    subscriber = await prisma.subscriber.create({
+      data: {
+        email: 'test@test.com',
+      },
+    });
+
+    expect(subscriber.email).toBe('test@test.com')
+  });
+
+  it('should create a topic action', async () => {
+    expect(await service.create({
+      topicId: topic.id,
+      subscriberId: subscriber.id,
+      action: 'subscribed',
+    })).toMatchObject({ action: 'subscribed' });
   });
 });
--- blog-backend/src/topic-actions/topic-actions.service.ts
+++ blog-backend/src/topic-actions/topic-actions.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { TopicAction } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateTopicActionInput } from './dto/create-topic-action.input';
 
 @Injectable()
 export class TopicActionsService {
   constructor(private prisma: PrismaService) { }
-  create(createTopicActionInput: CreateTopicActionInput) {
+  create(createTopicActionInput: CreateTopicActionInput): Promise<TopicAction> {
     return this.prisma.topicAction.create({
       data: createTopicActionInput,
     });
